92df2157a18b6e30e65380b871e2b5834234b01c
Aaditya Pareek <51163131+AmazingAaditya@users.noreply.github.com>
2022-04-11 17:59:47 +0530

    Link edited

--- src/Sections/Event/Event.jsx
+++ src/Sections/Event/Event.jsx
@@ -61,7 +61,7 @@ function Event() {
                 <div data-aos="flip-right">
                   <EventCard
                     head="VisionX"
-                    link="https://bit.ly/ImpetusRegistartion"
+                    link="https://bit.ly/Vision-XRegistration"
                     tagline="Envisioning a utopian odyssey of pioneering technology"
                     reg="start"
                     content="Visualize a single platform, with all the bibles of Business accessible. 
